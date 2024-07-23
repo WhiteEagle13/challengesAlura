858037fe96fbc3351824913d088db7fc1cd40f21
Augusto Schmidt <ECIUTR5@johndeere.com>
2024-07-23 11:13:28 -0300

    Botão adicionar criado (carrinho-compras)

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,11 @@
+function adicionar() {
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<\section class="carrinho__produtos__produto">
+    <span  class="texto-azul"> ${quantidade} </span> ${nomeProduto} <span class="texto-azul"> ${preco} </span>
+  </section>`;
+}
